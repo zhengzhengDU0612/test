975fcbde19ad8e99996a27a45bf210f035cf7a52
zhengzhengDU0612 <1053833425@qq.com>
2019-12-29 20:46:53 +0800

    push current files

---
+++ src/main/java/com/pt/helloworld/PtApplication.java
@@ -0,0 +1,13 @@
+package com.pt.helloworld;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtApplication {
+    public static void main(String[] args){
+        //Spring应用启动起来
+        SpringApplication.run(PtApplication.class,args);
+    }
+}
