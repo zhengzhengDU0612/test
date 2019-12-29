140925f87c8ff1e42c030544019c6b4b3c7dd305
zhengzhengDU0612 <10053833425@qq.com>
2019-12-29 23:29:27 +0800

    testdddd

--- src/main/java/com/pt/helloworld/controller/HelloWorld.java
+++ src/main/java/com/pt/helloworld/controller/HelloWorld.java
@@ -1,12 +1,20 @@
 package com.pt.helloworld.controller;
 
 
+import com.pt.helloworld.service.HelloWorldService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
+
 @Controller
 public class HelloWorld {
+
+    @Resource
+    private HelloWorldService helloWorldService;
+
+
     @ResponseBody
     @RequestMapping("/hello")
     public  String hello() {
