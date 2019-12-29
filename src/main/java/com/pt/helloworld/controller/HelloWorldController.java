689bb5748f16bdeaced5bd25e36c7f29e8119506
zhengzhengDU0612 <10053833425@qq.com>
2019-12-29 22:13:10 +0800

    test

--- src/main/java/com/pt/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/pt/helloworld/controller/HelloWorldController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HelloWorld {
+public class HelloWorldController {
     @ResponseBody
     @RequestMapping("/hello")
     public  String hello() {
